d03ceb995bbe4dec700d620d9ce7a885826b34d8
iker777 <ikeralvarez777@gmail.com>
2022-07-28 11:24:52 +0200

    Arreglando el pull request de angelo y Carlos

--- js/script.js
+++ js/script.js
@@ -1,40 +1,29 @@
 const carlos = document.querySelector(".carlos")
-
-
-function ColorTexto(){
-    const respuestaUsuario = prompt("Elige tu color") 
-    document.body.style.backgroundColor=respuestaUsuario
-    let parrafo =document.createElement("p")
-    parrafo.style.color = "blue"
-    parrafo.innerHTML = prompt("Introduce la palabra")
-    carlos.append(parrafo)
-
-}
-
-
-
-
-
-carlos.addEventListener("click",ColorTexto)
-
-
+let color = document.getElementById("color")
+let color2="detente ladron!";
 let angelo = document.querySelector(".angelo")
 let fondo = document.querySelector("div")
 
+function ColorTexto(){
+  const respuestaUsuario = prompt("Elige tu color") 
+  document.body.style.backgroundColor=respuestaUsuario
+  let parrafo =document.createElement("p")
+  parrafo.style.color = "blue"
+  parrafo.innerHTML = prompt("Introduce la palabra")
+  carlos.after(parrafo)
+}
 function cambio(){
-
-    if(fondo.style.backgroundColor == "red") {
-        fondo.style.backgroundColor="blue";
-    }else{
-        fondo.style.backgroundColor = "red";
-    }   
+  
+  if(fondo.style.backgroundColor == "red") {
+    fondo.style.backgroundColor="blue";
+  }else{
+    fondo.style.backgroundColor = "red";
+  }   
 }
-
-angelo.addEventListener("click",cambio)
-
-let color = document.getElementById("color")
-let color2="detente ladron!";
-
 function cambiarColor(){
-    color.innerHTML = color2
+  color.innerHTML = color2
 }
+
+carlos.addEventListener("click",ColorTexto);
+angelo.addEventListener("click",cambio);
+angelo.addEventListener("click",cambiarColor);
