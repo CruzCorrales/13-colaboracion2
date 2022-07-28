90858e92dc6423ed8bec880007adc3ede53c5e98
Dakardu <unodago@gmail.com>
2022-07-28 10:59:26 +0200

    Cambio imagen Dago

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 const btnDago = document.querySelector('.dago')
+
 console.log(btnDago)
 
 function cambiarColor(){
     document.body.style.backgroundColor = 'rgb(193, 193, 246)'
 }
 function cargarImagen(){
+    const mainDago = document.querySelector('.main')
     const div = document.createElement('div');
-    div.innerHTML = '<img src="./img/tecnologia2.jpg">'
+    div.innerHTML = '<img src="./img/programar.jpg">'
     div.className = 'img'
-    document.body.append(div)
+    mainDago.append(div)
 }
-
 
 btnDago.addEventListener('click', cambiarColor)
 btnDago.addEventListener('click', cargarImagen)
